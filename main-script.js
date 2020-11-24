d794a8ad31c541260ff8a147c985b15307075df9
Thomas <thomasguizzetti@hotmail.it>
2020-11-24 11:29:07 +0000

    Links added to GitHub, Codewars and FreecodeCamp divs

--- main-script.js
+++ main-script.js
@@ -4,6 +4,11 @@ var buttonThree = document.querySelector("#buttonThree");
 var tabOne = document.querySelector("#tabOne");
 var tabTwo = document.querySelector("#tabTwo");
 var tabThree = document.querySelector("#tabThree");
+var GitButton = document.querySelector("#GitButton");
+var FreeCodeCampButton = document.querySelector("#FreeCodeCamp");
+var CodeWarsButton = document.querySelector("#CodeWarsButton");
+var GitCodeFreeButtons = document.querySelectorAll(".GitCodeFreeButtons");
+
 
 buttonOne.addEventListener("click", function(){
     tabOne.style.visibility = "visible";
@@ -23,4 +28,30 @@ buttonThree.addEventListener("click", function(){
     tabThree.style.visibility = "visible";
     tabFour.style.visibility = "hidden";
 });
+
+//GitHub Button - Onlick event
+GitCodeFreeButtons[0].addEventListener("click", function(){
+        GitCodeFreeButtons[0].style.backgroundColor = "black";
+        GitCodeFreeButtons[1].style.backgroundColor = "#333A5E";
+        GitCodeFreeButtons[2].style.backgroundColor = "#333A5E";
+});
+
+//CodeWars Button - onlick event
+GitCodeFreeButtons[1].addEventListener("click", function(){
+    GitCodeFreeButtons[1].style.backgroundColor = "black";
+    GitCodeFreeButtons[0].style.backgroundColor = "#333A5E";
+    GitCodeFreeButtons[2].style.backgroundColor = "#333A5E";
+});
+
+//FreeCodeCamp Button - onlick event
+GitCodeFreeButtons[2].addEventListener("click", function(){
+    GitCodeFreeButtons[2].style.backgroundColor = "black";
+    GitCodeFreeButtons[1].style.backgroundColor = "#333A5E";
+    GitCodeFreeButtons[0].style.backgroundColor = "#333A5E";
+});
+
+
+
+
+
 

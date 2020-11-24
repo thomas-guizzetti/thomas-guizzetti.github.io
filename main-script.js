f3ad44607a59932057f0ae24359ee5de0d494829
Thomas <thomasguizzetti@hotmail.it>
2020-11-24 11:50:58 +0000

    Lower position for GitHub, Codewars and FreeCodeCamp divs

--- main-script.js
+++ main-script.js
@@ -31,23 +31,23 @@ buttonThree.addEventListener("click", function(){
 
 //GitHub Button - Onlick event
 GitCodeFreeButtons[0].addEventListener("click", function(){
-        GitCodeFreeButtons[0].style.backgroundColor = "black";
-        GitCodeFreeButtons[1].style.backgroundColor = "#333A5E";
-        GitCodeFreeButtons[2].style.backgroundColor = "#333A5E";
+        GitCodeFreeButtons[0].style.borderColor = "black";
+        GitCodeFreeButtons[1].style.borderColor = "#333A5E";
+        GitCodeFreeButtons[2].style.borderColor = "#333A5E";
 });
 
 //CodeWars Button - onlick event
 GitCodeFreeButtons[1].addEventListener("click", function(){
-    GitCodeFreeButtons[1].style.backgroundColor = "black";
-    GitCodeFreeButtons[0].style.backgroundColor = "#333A5E";
-    GitCodeFreeButtons[2].style.backgroundColor = "#333A5E";
+    GitCodeFreeButtons[1].style.borderColor = "white";
+    GitCodeFreeButtons[0].style.borderColor = "#333A5E";
+    GitCodeFreeButtons[2].style.borderColor = "#333A5E";
 });
 
 //FreeCodeCamp Button - onlick event
 GitCodeFreeButtons[2].addEventListener("click", function(){
-    GitCodeFreeButtons[2].style.backgroundColor = "black";
-    GitCodeFreeButtons[1].style.backgroundColor = "#333A5E";
-    GitCodeFreeButtons[0].style.backgroundColor = "#333A5E";
+    GitCodeFreeButtons[2].style.borderColor = "white";
+    GitCodeFreeButtons[1].style.borderColor = "#333A5E";
+    GitCodeFreeButtons[0].style.borderColor = "#333A5E";
 });
 
 

1fd72bf00f3e6ddfd13e26c79428eb18523de1a9
Thomas <thomasguizzetti@hotmail.it>
2020-11-30 19:58:49 +0000

    Add submit button on last tab

--- main-script.js
+++ main-script.js
@@ -14,19 +14,16 @@ buttonOne.addEventListener("click", function(){
     tabOne.style.visibility = "visible";
     tabTwo.style.visibility = "hidden";
     tabThree.style.visibility = "hidden";
-    tabFour.style.visibility = "hidden";
 });
 buttonTwo.addEventListener("click", function(){
     tabOne.style.visibility = "hidden";
     tabTwo.style.visibility = "visible";
     tabThree.style.visibility = "hidden";
-    tabFour.style.visibility = "hidden";
 });
 buttonThree.addEventListener("click", function(){
     tabOne.style.visibility = "hidden";
     tabTwo.style.visibility = "hidden";
     tabThree.style.visibility = "visible";
-    tabFour.style.visibility = "hidden";
 });
 
 //GitHub Button - Onlick event
@@ -48,6 +45,17 @@ GitCodeFreeButtons[2].addEventListener("click", function(){
     GitCodeFreeButtons[2].style.borderColor = "white";
     GitCodeFreeButtons[1].style.borderColor = "#333A5E";
     GitCodeFreeButtons[0].style.borderColor = "#333A5E";
+});
+
+
+var artist = document.querySelectorAll("#artist");
+var song = document.querySelectorAll("#song");
+var musicForm = document.querySelectorAll("#musicForm");
+
+musicForm.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+    console.log("hello world!");
 });
 
 
